264402e23143cb24ae1a1a28e9c81b84bb4ecc8f
CameronBird <hellocameron2000@gmail.com>
2022-06-15 16:21:03 +0100

    Changed graph axis

--- public/js/graph.js
+++ public/js/graph.js
@@ -4,15 +4,15 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     // Set Data
     var data = google.visualization.arrayToDataTable([
-      ['Price', 'Size'],
+      ['Time', 'Temp'],
       [50,7],[60,8],[70,8],[80,9],[90,9],[100,9],
       [110,10],[120,11],[130,14],[140,14],[150,15]
       ]);
     // Set Options
     var options = {
-      title: 'House Prices vs Size',
-      hAxis: {title: 'Square Meters'},
-      vAxis: {title: 'Price in Millions'},
+      title: 'Daily Weather',
+      hAxis: {title: 'Time'},
+      vAxis: {title: 'Temperature C'},
       legend: 'none'
     };
     // Draw Chart
